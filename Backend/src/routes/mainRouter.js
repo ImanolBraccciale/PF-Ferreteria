c0f1aec183dcb0615d7446c5c584bed0bc3d465b
Imanol Bracciale <128749818+ImanolBraccciale@users.noreply.github.com>
2023-09-12 17:11:53 -0300

    Delete Backend directory
    esta era la version 12

--- Backend/src/routes/mainRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const usersRouter = require("./usersRouter");
-const postRouter = require("./postRouter");
-const mainRouter = Router();
-
-mainRouter.use("/users", usersRouter);
-mainRouter.use("/posts", postRouter);
-
-module.exports = mainRouter;
