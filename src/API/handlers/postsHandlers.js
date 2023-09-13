622e4b46950a225e82df8d53365cd86887397ed4
FelipeOcc <135179408+FelipeOcc@users.noreply.github.com>
2023-09-13 19:54:51 -0300

    Delete src/API directory

--- src/API/handlers/postsHandlers.js
+++
@@ -1,9 +0,0 @@
-const createPostHandler = (req, res) => {
-  res.status(200).send("Crear Usuario.");
-};
-
-const createPostItemHandler = (req, res) => {
-  res.status(200).send("Agregar un elemento");
-};
-
-module.exports = { createPostHandler, createPostItemHandler };
--- src/API/routes/postRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const {createPostHandler, createPostItemHandler} = require('../handlers/postsHandlers')
-const postRouter = Router();
-
-postRouter.post("/", createPostHandler);
-
-postRouter.post("/add", createPostItemHandler);
-
-module.exports = postRouter;
