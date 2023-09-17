d1dbde27e8a98bfb115236ba517dc29ecdb6e4de
Imanol Bracciale <imanol11699@outlook.com>
2023-09-17 01:31:10 -0300

    tag hecho

---
+++ src/pages/api/tag/index.js
@@ -0,0 +1,35 @@
+const postTag = require("../controllers/Tag/postTag")
+const deletTag = require("../controllers/Tag/deleteTag")
+const getTag = require("../controllers/Tag/getTag")
+export default async  function Handler(req, res){
+
+  switch (req.method) {
+    case "GET":
+      try {
+          const getTags = await getTag(req.body); 
+          return res.status(201).json(getTags);
+      } catch (error) {
+          return res.status(400).json({ error: error.message });
+      }
+    case "POST":
+      try {
+        console.log("prueba 1");
+        const newProduct = await postTag(req.body); 
+        return res.status(201).json(newProduct);
+      } catch (error) {
+        return res.status(400).json({ error: error.message });
+      }
+      case "DELETE":
+        try {
+        const {id} = req.body
+        const delTag = await deletTag(id); 
+        return res.status(201).json(delTag);
+      } catch (error) {
+        return res.status(400).json({ error: error.message });
+      }
+
+
+    default:
+      break;
+  }
+};
