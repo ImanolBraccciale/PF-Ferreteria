e38b203bd82410b5e53063177f760da5fed5a3d2
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-17 18:18:28 -0500

    "id de proveedores acomodado"

--- src/models/Suppliers.js
+++ src/models/Suppliers.js
@@ -3,36 +3,37 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-    const Suppliers = sequelize.define('Suppliers', {
-        id_suppliers: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  const Suppliers = sequelize.define(
+    "Suppliers",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-        cellphone: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+      cellphone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-        name_company: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+      name_company: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-        isActive: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
     },
     // Deshabilitamos timestamps (created_at y updated_at)
-    { timestamps: false });
-    
-    return Suppliers;
-    } 
- 
-  
+    { timestamps: false }
+  );
+
+  return Suppliers;
+};
