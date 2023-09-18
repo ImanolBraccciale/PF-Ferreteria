4218503138d007621a4ea24444508a957654603b
Daniel Agudelo <daniel.agudelo185@gmail.com>
2023-09-17 21:59:20 -0500

    desde developer actualizado
    actualizado

--- src/app/redux/actions/actionsTypes.js
+++ src/app/redux/actions/actionsTypes.js
@@ -1,10 +1,6 @@
 export const GET_SUPPLIERS = "GET_SUPPLIERS"
 export const POST_SUPPLIERS = "POST_SUPPLIERS"
 export const GET_BY_ID = "GET_BY_ID"
-<<<<<<< HEAD
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS"
 export const DELETE_DETAIL = "DELETE_DETAIL"
-=======
-export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
 export const ORDER_BY = "ORDER_BY";
->>>>>>> 76df8a3eb0ad196030b1559a5697d9c97350998f
