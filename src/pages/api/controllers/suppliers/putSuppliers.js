eac86e2c08c106e78227010568d98e77ce6f73b3
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-18 11:02:59 -0500

    comenzando

--- src/pages/api/controllers/suppliers/putSuppliers.js
+++ src/pages/api/controllers/suppliers/putSuppliers.js
@@ -2,10 +2,10 @@ const { db } = require("../../db");
 db.sequelize.sync();
 const Suppliers = db.Suppliers;
 
-module.exports = async (data) => {
-  const supplier = await Suppliers.findOne({ where: { id: data.id } });
+module.exports = async (id, info) => {
+  const supplier = await Suppliers.findOne({ where: { id } });
   if (!supplier) throw new Error("supplier not found");
 
-  const supplierUpdated = await supplier.update(data);
+  const supplierUpdated = await supplier.update(info);
   return supplierUpdated;
 };
