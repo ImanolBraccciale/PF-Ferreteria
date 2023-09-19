d69c29f855518cdebf6b2b33f44d4b0ec1333cf6
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-19 17:33:36 -0500

    "nodemailer funciona enviando correo a la ruta post"

---
+++ src/pages/api/service/mailService.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+//creamos un servicio que utilizará la biblioteca nodemailer para construir objetos de correo electrónico y enviar correos electrónicos
+async function sendMail(subject, toEmail, otpText) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
+    to: toEmail,
+    subject: subject,
+    text: otpText,
+  };
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email Sent:", info.messageId);
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Unable to send email.");
+  }
+}
+
+module.exports = { sendMail };
+
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       throw new Error(error);
+//     } else {
+//       console.log("Email Sent");
+//       return true;
+//     }
+//   });
+// }
