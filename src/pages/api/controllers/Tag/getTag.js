03f3469f8db921d10a9473706243989504deec98
Imanol Bracciale <imanol11699@outlook.com>
2023-09-20 04:10:23 -0300

    asdasdasda

--- src/pages/api/controllers/Tag/getTag.js
+++ src/pages/api/controllers/Tag/getTag.js
@@ -5,7 +5,6 @@ const Products = db.Products
 
 module.exports = async (tagName) => {
 
-<<<<<<< HEAD
  try {
    const tag = await Tag.findAll()
   // const tag = await Tag.findOne({
@@ -25,28 +24,5 @@ module.exports = async (tagName) => {
  } catch (error) {
   throw new Error({error:error.message})
  }
-=======
-  try {
-    const tag = await Tag.findAll(
-    //   {
-    //   where: {
-    //     name: tagName
-    //   }
-    // }
-    )
-
-    // if (tag) {
-    //   const products = await Tag.findAll({
-    //     where: {
-    //       TagId: tag.id
-    //     }
-    //   })
-    //   return products;
-    // }
-    return tag
-  } catch (error) {
-    throw new Error({ error: error.message })
-  }
->>>>>>> 9949d98c7eb97afc3ca6290f7a09a7d80d124dc3
 
 }
