4b2ee6f2663dde1009ba7dab723fe20580ec3778
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-26 10:22:10 -0500

    "dark mode desde layout"

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,23 +1,47 @@
-"use client"
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Provider } from 'react-redux'
-import store from './redux/store/store'
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const DarkModeButton = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
+  return (
+    <button onClick={toggleDarkMode}>
+      {darkMode ? "Desactivar Modo Oscuro" : "Activar Modo Oscuro"}
+    </button>
+  );
+};
 
 export const metadata = {
-  title: 'ROFE Ferretería'
-}
+  title: "ROFE Ferretería",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <Provider store={store}>
         <body className={inter.className}>
+          <DarkModeButton />
           {children}
         </body>
       </Provider>
-    </html >
-  )
+    </html>
+  );
 }
