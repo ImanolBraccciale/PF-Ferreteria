d82a8b3f6688c0b5517ea53da09ec8a05b378483
Imanol Bracciale <imanol11699@outlook.com>
2023-09-28 18:08:22 -0300

    arregle los bugs de update, por fin

--- src/pages/api/controllers/Productos/upProducts.js
+++ src/pages/api/controllers/Productos/upProducts.js
@@ -6,24 +6,57 @@ const Tag = db.Tag;
 module.exports = async (data) => {
   try {
     // Busca el producto por su ID
-    const product = await Products.findByPk(data.id, {
-      include: [
-        {
-          model: Suppliers,
-          as: 'supplier',
-        },
-        {
-          model: Tag,
-          through: { attributes: [] },
-        },
-      ],
-    });
+    const product = await Products.findByPk(data.id);
 
     if (!product) {
       throw new Error('El producto no fue encontrado');
     }
 
-    // Actualiza los campos del producto con los datos proporcionados
+    // Busca el proveedor existente por el nombre proporcionado
+    const existingSupplier = await Suppliers.findOne({
+      where: { name: data.supplierName },
+    });
+
+    if (!existingSupplier) {
+      throw new Error('Proveedor no encontrado en la base de datos');
+    }
+
+    // Utiliza setSupplier para cambiar el proveedor del producto
+    await product.setSupplier(existingSupplier);
+
+    // Inicializa una variable para almacenar las etiquetas existentes
+    let existingTags = [];
+
+    // Verifica si se proporcionó el nombre de grupo
+    if (data.group) {
+      // Busca la etiqueta de grupo existente por nombre
+      const existingGroupTag = await Tag.findOne({
+        where: { name: data.group, type: 'group' },
+      });
+
+      // Si se encuentra la etiqueta de grupo, agrégala a la lista de etiquetas existentes
+      if (existingGroupTag) {
+        existingTags.push(existingGroupTag);
+      }
+    }
+
+    // Verifica si se proporcionó el nombre de rubro
+    if (data.rubro) {
+      // Busca la etiqueta de rubro existente por nombre
+      const existingRubroTag = await Tag.findOne({
+        where: { name: data.rubro, type: 'rubro' },
+      });
+
+      // Si se encuentra la etiqueta de rubro, agrégala a la lista de etiquetas existentes
+      if (existingRubroTag) {
+        existingTags.push(existingRubroTag);
+      }
+    }
+
+    // Asocia las etiquetas existentes al producto
+    await product.setTags(existingTags);
+
+    // Actualiza otros campos del producto con los datos proporcionados
     product.name = data.name;
     product.descripcion = data.descripcion;
     product.costoAnterior = data.costoAnterior;
@@ -32,83 +65,33 @@ module.exports = async (data) => {
     product.stock = data.stock;
     product.isActive = data.isActive;
 
-    // Actualiza el proveedor si es necesario
-    if (data.supplierName) {
-      product.supplier.name = data.supplierName;
-      await product.supplier.save();
-    }
-
-    // Actualiza las etiquetas del producto
-    if (data.group && data.rubro) {
-      const tagNames = [data.group, data.rubro];
-      const tagInstances = await Tag.findAll({
-        where: { name: tagNames },
-      });
-
-      await product.setTags(tagInstances);
-    }
-
     // Guarda los cambios en el producto
     await product.save();
-
-
+    
     return product;
   } catch (error) {
-
     throw error;
   }
 };
 
 
-// const { db } = require("../../db");
-// const Products = db.Products;
-// const Suppliers = db.Suppliers;
-// const Tag = db.Tag;
+// const product = await Products.findByPk(data.id);
 
-// module.exports = async (data) => {
-//   try {
-//     // Busca el producto por su ID
-//     const product = await Products.findByPk(data.id);
+// if (!product) {
+//   throw new Error('El producto no fue encontrado');
+// }
 
+// // Busca el proveedor existente por el nombre proporcionado
+// const existingSupplier = await Suppliers.findOne({
+//   where: { name: data.supplierName },
+// });
 
+// if (!existingSupplier) {
+//   throw new Error('Proveedor no encontrado en la base de datos');
+// }
 
-//     // Actualiza los campos del producto con los datos proporcionados
-//     product.name = data.name;
-//     product.descripcion = data.descripcion;
-//     product.costoAnterior = data.costoAnterior;
-//     product.costoActual = data.costoActual;
-//     product.price = data.price;
-//     product.stock = data.stock;
-//     product.isActive = data.isActive;
+// // Utiliza setSupplier para cambiar el proveedor del producto
+// await product.setSupplier(existingSupplier);
 
-//     // Actualiza el proveedor si es necesario
-//     if (data.supplier && data.supplier.name) {
-//       const [supplier] = await Suppliers.findOrCreate({
-//         where: { name: data.supplier.name },
-//       });
-//       product.SupplierId = supplier.id;
-//     }
-
-//     // Actualiza las etiquetas del producto
-//     if (data.Tags && Array.isArray(data.Tags)) {
-//       const tagIds = [];
-//       for (const tagData of data.Tags) {
-//         const [tag] = await Tag.findOrCreate({
-//           where: { name: tagData.name },
-//           defaults: { type: tagData.type },
-//         });
-//         tagIds.push(tag.id);
-//       }
-//       product.setTags(tagIds);
-//     }
-
-//     // Guarda los cambios en la base de datos
-//     await product.save();
-
-//     console.log('Producto actualizado con éxito:', product);
-//     return product;
-//   } catch (error) {
-//     console.error('Error al actualizar el producto:', error.message);
-//     throw error;
-//   }
-// };
+// // Guarda los cambios en el producto
+// await product.save();
