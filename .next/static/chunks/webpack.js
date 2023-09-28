4480bb84fa76f3392732398f3e711fa2e77bd026
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-28 10:39:41 -0500

    "mejoras estilos dark mode"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "270e14aefa623ec4"; }
+/******/ 		__webpack_require__.h = function() { return "71599c2d53a64ab9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
