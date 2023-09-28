001c7c8ab380ea8a54b1572ef477e610e2c33890
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-28 14:01:13 -0500

    "a"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1c3c865f33857ec"; }
+/******/ 		__webpack_require__.h = function() { return "34415ed08069830d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/componentes/ProductList/ProductList.jsx
+++ src/app/componentes/ProductList/ProductList.jsx
@@ -1,33 +1,21 @@
-import s from '@/app/componentes/ProductList/ProductList.module.css'
+import s from "@/app/componentes/ProductList/ProductList.module.css";
 
-function ProductList({id, name,stock, costoActual,price}) {
-    return (
-        <div className={s.all}>
-            <div className={s.container}>
-                <div className={s.a}>
-                    {id}
-                </div>
-                <div className={s.b}>
-                    {name}
-                </div>
-                <div className={s.c}>
-                    {stock}
-                </div>
-                <div className={s.d}>
-                    {costoActual}
-                </div>
-                <div className={s.e}>
-                    {(costoActual * 1.21)}
-                </div>
-                <div className={s.d}>
-                    {price}
-                </div>
-                <div className={s.f}>
-                    <button className={s.button}>Agregar</button>
-                </div>
-            </div>
+function ProductList({ id, name, stock, costoActual, price }) {
+  return (
+    <div className={s.all}>
+      <div className={s.container}>
+        <div className={s.a}>{id}</div>
+        <div className={s.b}>{name}</div>
+        <div className={s.c}>{stock}</div>
+        <div className={s.d}>{costoActual}</div>
+        <div className={s.e}>{costoActual * 1.21}</div>
+        <div className={s.d}>{price}</div>
+        <div className={s.f}>
+          <button className={s.button}>Detalle</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ProductList
+export default ProductList;
