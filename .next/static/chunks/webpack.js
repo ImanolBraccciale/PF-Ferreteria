46e573ba638e2dd8755d27c5da6beb349c300751
Amderson Hernandez <ahernandezdep@gmail.com>
2023-09-29 11:39:20 -0500

    ""Mejorando detalle Dark"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50af088c59a094a3"; }
+/******/ 		__webpack_require__.h = function() { return "014cb17ebce606d8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
