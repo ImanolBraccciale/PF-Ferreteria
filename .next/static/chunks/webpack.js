809738c6cc8c73671c375f5d9b36d996f97b63c4
Imanol Bracciale <imanol11699@outlook.com>
2023-09-29 17:37:22 -0300

    form tag ahora acepta y envia al back el type(si es rubro o  grupo)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "014cb17ebce606d8"; }
+/******/ 		__webpack_require__.h = function() { return "49d58fd1edbb3361"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
