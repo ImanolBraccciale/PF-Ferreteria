b779fd8c1e1d410fcb70aa1dfbc22beefeb9991e
Daniel Agudelo <daniel.agudelo185@gmail.com>
2023-09-29 15:48:08 -0500

    actualizacion de rama

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af235f6896704fb0"; }
+/******/ 		__webpack_require__.h = function() { return "25a2efb11272d839"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
