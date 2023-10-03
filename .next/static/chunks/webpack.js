630f88f3e2646e77c4b2ac0ff881838e128972a3
Imanol Bracciale <imanol11699@outlook.com>
2023-10-03 19:52:13 -0300

    imagen se actualiza, arregla el undefinied despues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "719919527041e37c"; }
+/******/ 		__webpack_require__.h = function() { return "b00fbdfe48dd5c66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
