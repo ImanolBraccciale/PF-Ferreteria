4ea751dd893565a34eed58bc4b1768bcdb74dc17
Vanessa Gustin <vanesagustin98@hotmail.com>
2023-10-02 23:20:45 -0500

    borado logico de products y suppliers

--- src/pages/api/controllers/suppliers/deleteSuppliers.js
+++ src/pages/api/controllers/suppliers/deleteSuppliers.js
@@ -1,11 +1,11 @@
 import { db } from "../../db";
 const { Suppliers } = db;
 
-const deleteSuppliers = async (id) => {
-  const supplier = await Suppliers.findByPk(id);
+const deleteSuppliers = async (id_suppliers) => {
+  const supplier = await Suppliers.findByPk(id_suppliers);
   if (!supplier) throw new Error("unknow Supplier");
   await Suppliers.destroy({
-    where: { id: id },
+    where: { id_suppliers },
   });
   return supplier;
 };
