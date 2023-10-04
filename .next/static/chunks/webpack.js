d8984adf2eb01970704d95ac7e038e418738a3be
Imanol Bracciale <imanol11699@outlook.com>
2023-10-04 08:20:24 -0300

    history ready

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad1b725da10ba37b"; }
+/******/ 		__webpack_require__.h = function() { return "9b57562a34819a9e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/formCarrito/page.jsx
+++ src/app/formCarrito/page.jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import style from "./page.module.css";
-
+import { useState } from 'react';
 import NavBar from "../componentes/NavBar/NavBar";
 import BackButtom from "../componentes/BackButtom/BackButtom";
 import ProductListCart from "../componentes/ProductListCart/ProductListCart";
@@ -15,7 +15,7 @@ function FormCarrito() {
   const dispatch = useDispatch();
   const allCartItems = useSelector((state) => state.allCartItems);
   console.log("allCartItems", allCartItems);
-
+  const [paymentMethod, setPaymentMethod] = useState("efectivo");
   const productSummary = [];
 
   allCartItems.forEach((item) => {
@@ -43,17 +43,20 @@ function FormCarrito() {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (productSummary.length === 0) {
-      alert(
-        "Debe agregar al menos un producto al carrito para poder generar la compra"
-      );
+      alert("Debe agregar al menos un producto al carrito para poder generar la compra");
     } else {
-      dispatch(postSale(productSummary)).then((data) => {
+      const dataToSend = {
+        productSummary,
+        paymentMethod,
+      };
+      console.log(dataToSend);
+      dispatch(postSale(dataToSend)).then((data) => {
         if (typeof data.payload === "object") {
-         alert("Su compra se realizó existósamente.")
-        //  Se redirecciona por window a / y se limpia el carrito
-         location.href = "/";
+          alert("Su compra se realizó existósamente.");
+          // Redirecciona por ventana a la página principal y limpia el carrito
+          window.location.href = "/";
         } else {
-          alert("Hebo un error al generar su compra, por favor inténtelo nuevamente.")
+          alert("Hubo un error al generar su compra, por favor inténtelo nuevamente.");
         }
       });
     }
@@ -79,6 +82,11 @@ function FormCarrito() {
           );
         }
       )}
+      <select name="paymentMethod" onChange={(e) => setPaymentMethod(e.target.value)}>
+        <option value="efectivo">Efectivo</option>
+        <option value="tarjeta">Tarjeta</option>
+        <option value="mercadoPago">MercadoPago</option>
+      </select>
       <Link href="/">
         <BackButtom />
       </Link>
