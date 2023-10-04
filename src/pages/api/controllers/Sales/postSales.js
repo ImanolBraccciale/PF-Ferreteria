dd7fe3db5535fdb67bc07fb27d5b1247ea4fefe8
sergioortegacastillo <sergio.ortegacastillo@hotmail.com>
2023-10-03 22:51:31 -0300

    Descontar stock al generar una venta

--- src/pages/api/controllers/Sales/postSales.js
+++ src/pages/api/controllers/Sales/postSales.js
@@ -4,6 +4,7 @@ db.sequelize.sync();
 const SalePayments = db.SalePayments;
 const SaleDetails = db.SaleDetails;
 const SaleMaster = db.SaleMaster;
+const Products = db.Products;
 
 module.exports = async (data) => {
   let totalAmout = 0;
@@ -30,8 +31,27 @@ module.exports = async (data) => {
       amount: totalAmout,
     });
 
+    data.map(async (e) => {
+      const getStock = await Products.findOne({
+        where: {
+          id: e.ID,
+        },
+      });
+      let nuevoStock = parseInt(getStock.dataValues.stock) - parseInt(e.Qty);
+      const [numFilasActualizadas] = await Products.update(
+        { stock: nuevoStock },
+        {
+          where: { id: e.ID },
+        }
+      );
+      console.log("numFilasActualizadas", numFilasActualizadas);
+    });
+
+    // const { stock } = getStock.dataValues;
+    // console.log("Stock", getStock, stock);
+
     return newSale;
   } catch (error) {
-    throw new Error({error:error.message})
+    throw new Error({ error: error.message });
   }
 };
