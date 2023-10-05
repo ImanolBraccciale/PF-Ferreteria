a01927ebb70aad354455f066c36d5a23628ccce9
Imanol Bracciale <imanol11699@outlook.com>
2023-10-05 12:58:12 -0300

    aaaaaaaaaaa

--- src/app/dashAdmin/graficos/BarsChart.js
+++ src/app/dashAdmin/graficos/BarsChart.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {  useSelector } from "react-redux";
+import axios from "axios";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -25,7 +25,6 @@ ChartJS.register(
 );
 
 export default function Bars() {
-  const allProducts = useSelector((state) => state.products);
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [
@@ -38,11 +37,22 @@ export default function Bars() {
   });
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/products");
+        const datos = response.data;
+        console.log("stock ", datos);
+        mostrar(datos);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     const mostrar = (datos) => {
       if (Array.isArray(datos)) {
         const labels = [];
         const data = [];
-        datos.filter((product) => product.isActive).forEach((element) => {
+        datos.filter(product => product.isActive).forEach((element) => {
           labels.push(element.name);
           data.push(element.costoActual);
         });
@@ -54,8 +64,8 @@ export default function Bars() {
       }
     };
 
-    mostrar(allProducts);
-  }, [allProducts]);
+    fetchData();
+  }, []); // useEffect se ejecuta solo una vez al montar el componente
 
   const misoptions = {
     responsive: true,
--- src/app/dashAdmin/graficos/PiesChart.js
+++ src/app/dashAdmin/graficos/PiesChart.js
@@ -1,71 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import axios from "axios";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler
-);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Pies() {
-  const allProducts = useSelector((state) => state.products);
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [
       {
-        label: "Ventas",
+        label: "Stock de productos",
         data: [],
-        backgroundColor: "rgba(220, 95, 0, 0.7)",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
       },
     ],
   });
 
   useEffect(() => {
-    const mostrar = (productos) => {
-      if (Array.isArray(productos)) {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/products");
+        const datos = response.data;
+        console.log(typeof datos);
+        console.log('circulo ',datos);
+        mostrar(datos);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const mostrar = (datos) => {
+      if (Array.isArray(datos)) {
         const labels = [];
         const data = [];
-        productos.filter((product) => product.isActive).forEach((element) => {
+        datos.filter(product => product.isActive).forEach((element) => {
           labels.push(element.name);
           data.push(element.stock);
         });
         setChartData({
           ...chartData,
           labels: labels,
-          datasets: [{ ...chartData.datasets[0], data: data }],
+          datasets: [
+            {
+              ...chartData.datasets[0],
+              data: data,
+            },
+          ],
         });
       }
     };
 
-    mostrar(allProducts);
-  }, [allProducts, chartData]);
+    fetchData();
+  }, []); // useEffect se ejecuta solo una vez al montar el componente
 
-  const misoptions = {
+  let misoptions = {
     responsive: true,
-    animation: false,
-    scales: {
-      y: {
-        min: 0,
-        max: 500,
-      },
-      x: {
-        ticks: { color: "rgba(220, 95, 0)" },
-      },
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
+    maintainAspectRatio: false,
   };
-//aaaaaaaaaaaa
 
   return <Pie data={chartData} options={misoptions} />;
 }
