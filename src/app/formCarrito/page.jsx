e6626eda0a3fe65f1431183a0ac55b79131fded6
sergioortegacastillo <sergio.ortegacastillo@hotmail.com>
2023-10-05 01:19:19 -0300

    Agregar pago mercadopago

--- src/app/formCarrito/page.jsx
+++ src/app/formCarrito/page.jsx
@@ -1,9 +1,9 @@
 "use client";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import style from "./page.module.css";
-import { useState } from 'react';
+import { useState } from "react";
 import NavBar from "../componentes/NavBar/NavBar";
 import BackButtom from "../componentes/BackButtom/BackButtom";
 import ProductListCart from "../componentes/ProductListCart/ProductListCart";
@@ -11,12 +11,19 @@ import ProductBarCart from "../componentes/ProductBarCart/ProductBarCart";
 
 import { postSale } from "../redux/actions/actions";
 
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import axios from "axios";
+import s from "./page.module.css";
+
+initMercadoPago("TEST-2704ea0d-a660-4fe8-be61-e2c32675c7ed");
+
 function FormCarrito() {
   const dispatch = useDispatch();
   const allCartItems = useSelector((state) => state.allCartItems);
   console.log("allCartItems", allCartItems);
   const [paymentMethod, setPaymentMethod] = useState("efectivo");
   const productSummary = [];
+  const [preferenceId, setPreferenceId] = useState(null);
 
   allCartItems.forEach((item) => {
     const productName = item.Name;
@@ -43,22 +50,52 @@ function FormCarrito() {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (productSummary.length === 0) {
-      alert("Debe agregar al menos un producto al carrito para poder generar la compra");
+      alert(
+        "Debe agregar al menos un producto al carrito para poder generar la compra"
+      );
     } else {
-      const dataToSend = {
-        productSummary,
-        paymentMethod,
-      };
-      console.log(dataToSend);
-      dispatch(postSale(dataToSend)).then((data) => {
-        if (typeof data.payload === "object") {
-          alert("Su compra se realizó existósamente.");
-          // Redirecciona por ventana a la página principal y limpia el carrito
-          window.location.href = "/";
-        } else {
-          alert("Hubo un error al generar su compra, por favor inténtelo nuevamente.");
+      if (paymentMethod === "mercadoPago") {
+        console.log("Pagar con mercado pago");
+        const subtotalsSum = productSummary.reduce(
+          (total, item) => total + item.Subtotal,
+          0
+        );
+        try {
+          console.log("create");
+          const response = await axios.post("/api/mercadoPago", {
+            product: {
+              description: "Venta general",
+              price: subtotalsSum,
+              quantity: 1,
+              currency_id: "ARS",
+            },
+          });
+
+          const { id } = response.data;
+          console.log("sample");
+          if (id) {
+            setPreferenceId(id);
+          }
+        } catch (error) {
+          console.error("Error al crear la preferencia:", error);
         }
-      });
+      } else {
+        const dataToSend = {
+          productSummary,
+          paymentMethod,
+        };
+        dispatch(postSale(dataToSend)).then((data) => {
+          if (typeof data.payload === "object") {
+            alert("Su compra se realizó existósamente.");
+            // Redirecciona por ventana a la página principal y limpia el carrito
+            window.location.href = "/";
+          } else {
+            alert(
+              "Hubo un error al generar su compra, por favor inténtelo nuevamente."
+            );
+          }
+        });
+      }
     }
   };
 
@@ -82,11 +119,19 @@ function FormCarrito() {
           );
         }
       )}
-      <select name="paymentMethod" onChange={(e) => setPaymentMethod(e.target.value)}>
+      <select
+        name="paymentMethod"
+        onChange={(e) => setPaymentMethod(e.target.value)}
+      >
         <option value="efectivo">Efectivo</option>
         <option value="tarjeta">Tarjeta</option>
         <option value="mercadoPago">MercadoPago</option>
       </select>
+      {preferenceId && (
+        <div>
+          <Wallet initialization={{ preferenceId }} className={s.wallet} />
+        </div>
+      )}
       <Link href="/">
         <BackButtom />
       </Link>
--- src/pages/api/sales/index.js
+++ src/pages/api/sales/index.js
@@ -1,33 +1,29 @@
-const postSale = require("../controllers/Sales/postSales")
-const getAllSales = require("../controllers/Sales/getAllSales")
-const getSalesById= require("../controllers/Sales/getSalesById")
-export default async  function Handler(req, res){
-
+const postSale = require("../controllers/Sales/postSales");
+const getAllSales = require("../controllers/Sales/getAllSales");
+const getSalesById = require("../controllers/Sales/getSalesById");
+export default async function Handler(req, res) {
   switch (req.method) {
-
     case "POST":
       try {
-        const newSale = await postSale(req.body)
-        return res.status(201).json(newSale)
+        const newSale = await postSale(req.body);
+        return res.status(201).json(newSale);
       } catch (error) {
         return res.status(400).json({ error: error.message });
       }
-          case "GET":
+    case "GET":
       try {
-         const { id } = req.query;
-         if (id) {
+        const { id } = req.query;
+        if (id) {
           const detailId = await getSalesById(id);
           return res.status(200).json(detailId);
         } else {
-        const allSale = await getAllSales()
-        return res.status(201).json(allSale)
+          const allSale = await getAllSales();
+          return res.status(201).json(allSale);
         }
-
- 
       } catch (error) {
         return res.status(400).json({ error: error.message });
       }
     default:
       break;
   }
-};
+}
