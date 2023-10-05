195dbc442981575c38ff510b290efafd1939b539
Imanol Bracciale <imanol11699@outlook.com>
2023-10-05 04:08:14 -0300

    segundo deploy

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -8,22 +8,25 @@ const SuppliersModel = require("../../models/Suppliers");
 const TagModel = require("../../models/Tag");
 const ReviewModel = require("../../models/Review");
 
-const SaleMasterModel = require("../../models/saleMaster");
-const SaleDetailsModel = require("../../models/saleDetails");
-const SalePaymentsModel = require("../../models/salePayments");
+const SaleMasterModel = require("../../models/saleMaster")
+const SaleDetailsModel = require("../../models/saleDetails")
+const SalePaymentsModel = require("../../models/salePayments")
 
 //IMPORTANTE!!!!!
 // const { faTruckMedical } = require('@fortawesome/free-solid-svg-icons');
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE } = process.env;
+const sequelize = new Sequelize(POSTGRES_DATABASE, POSTGRES_USER, POSTGRES_PASSWORD, {
+  host: POSTGRES_HOST,
   dialect: "postgres",
   dialectModule: require("pg"),
   force: false,
   operatorAliases: false,
   logging: false,
   native: false,
+  dialectOptions: {
+      ssl: true, 
+    },
   pool: {
     max: 5,
     min: 0,
@@ -43,29 +46,19 @@ db.Suppliers = SuppliersModel(sequelize);
 db.Tag = TagModel(sequelize);
 db.Review = ReviewModel(sequelize);
 
-db.SaleMaster = SaleMasterModel(sequelize);
-db.SaleDetails = SaleDetailsModel(sequelize);
-db.SalePayments = SalePaymentsModel(sequelize);
+db.SaleMaster = SaleMasterModel(sequelize)
+db.SaleDetails = SaleDetailsModel(sequelize)
+db.SalePayments = SalePaymentsModel(sequelize)
 
-const {
-  Users,
-  Products,
-  DetailSale,
-  Sale,
-  Suppliers,
-  Tag,
-  Review,
-  SaleMaster,
-  SaleDetails,
-  SalePayments,
-} = db.sequelize.models;
+const { Users, Products, DetailSale, Sale, Suppliers, Tag, Review, SaleMaster, SaleDetails, SalePayments} =
+  db.sequelize.models;
 
 Users.belongsToMany(Products, { through: "user_product", timestamps: false });
 Products.belongsToMany(Users, { through: "user_product", timestamps: false });
 
 Products.belongsTo(Suppliers, {
-  as: "supplier",
-  foreignKey: "SupplierId",
+  as: 'supplier',
+  foreignKey: 'SupplierId',
   allowNull: true,
 });
 
@@ -77,11 +70,11 @@ DetailSale.belongsToMany(Products, {
   through: "product_detailSale",
   timestamps: false,
 });
-SaleMaster.hasMany(SaleDetails, { foreignKey: "sale_id" });
-SaleMaster.hasMany(SalePayments, { foreignKey: "sale_id" });
+SaleMaster.hasMany(SaleDetails, { foreignKey: 'sale_id' });
+SaleMaster.hasMany(SalePayments, { foreignKey: 'sale_id' });
 
-SaleDetails.belongsTo(SaleMaster, { foreignKey: "sale_id" });
-SalePayments.belongsTo(SaleMaster, { foreignKey: "sale_id" });
+SaleDetails.belongsTo(SaleMaster, { foreignKey: 'sale_id' });
+SalePayments.belongsTo(SaleMaster, { foreignKey: 'sale_id' });
 Products.belongsToMany(Tag, {
   through: "ProductTag",
   foreignKey: "productId",
@@ -91,14 +84,15 @@ Tag.belongsToMany(Products, {
   foreignKey: "tagId",
 });
 
-Sale.hasMany(DetailSale, { foreignKey: "saleId" });
-DetailSale.belongsTo(Sale, { foreignKey: "saleId" });
+Sale.hasMany(DetailSale, { foreignKey: 'saleId' });
+DetailSale.belongsTo(Sale, { foreignKey: 'saleId' });
 
-Review.belongsTo(Users, { foreignKey: "idUser" });
-Users.hasOne(Review, { foreignKey: "idUser" });
+Review.belongsTo(Users, { foreignKey: 'idUser'});
+Users.hasOne(Review, { foreignKey: 'idUser'});
 
-Review.hasOne(Sale, { foreignKey: "idReview" });
-Sale.belongsTo(Review, { foreignKey: "idReview" });
+Review.hasOne(Sale, { foreignKey: 'idReview' });
+Sale.belongsTo(Review, { foreignKey: 'idReview' });
+
 
 db.sequelize.sync();
 
